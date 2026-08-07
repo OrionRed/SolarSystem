fb01a12f8849f649368e0d4d3b393cc4331ea813
OrionRed <46463407+OrionRed@users.noreply.github.com>
2026-08-06 23:19:19 -0400

    modbus ready, next step is talking to rs485 to ttl board

--- components/modbus/modbus.c
+++ components/modbus/modbus.c
@@ -27,7 +27,12 @@ size_t modbus_build_read_request(
     frame[4] = register_count >> 8;
     frame[5] = register_count & 0xFF;
 
-    return 6;
+    uint16_t crc = modbus_crc(frame, 6);
+
+    frame[6] = crc & 0xFF;
+    frame[7] = crc >> 8;
+
+    return 8;
 }
 
 void modbus_self_test(void)
@@ -41,6 +46,9 @@ void modbus_self_test(void)
         2,
         frame);
 
+    ESP_LOGI(TAG, "frame = %02X %02X %02X %02X %02X %02X %02X %02X",
+        frame[0], frame[1], frame[2], frame[3], frame[4], frame[5], frame[6], frame[7]);
+        
     hal_uart_write(frame, length);
 
     ESP_LOGI(TAG, "Frame sent");
@@ -49,7 +57,7 @@ void modbus_self_test(void)
 uint16_t modbus_crc(
     const uint8_t *data,
     size_t length)
-{
+{    
     uint16_t crc = 0xFFFF;
 
     for (size_t i = 0; i < length; i++)
@@ -57,12 +65,13 @@ uint16_t modbus_crc(
         crc ^= data[i];
         for (int bit = 0; bit < 8; bit++)
         {
-            if (crc & 1)
+            bool lsb = crc & 1;
+
+            crc >>= 1;
+
+            if (lsb)
             {
-            }
-            else
-            {
-                crc >>= 1;
+                crc ^= 0xA001;
             }
         }
     }

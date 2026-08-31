b3c471f243a753aec129134e35bc3972ae30e176
OrionRed <46463407+OrionRed@users.noreply.github.com>
2026-08-30 20:47:24 -0400

    Initialize NVS before Wi-Fi

--- main/main.c
+++ main/main.c
@@ -1,8 +1,27 @@
 #include "app.h"
 #include "wifi.h"
 
+#include "esp_err.h"
+#include "nvs_flash.h"
+
+static void nvs_init(void)
+{
+    esp_err_t ret = nvs_flash_init();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
+    else
+    {
+        ESP_ERROR_CHECK(ret);
+    }
+}
+
 void app_main(void)
 {
+    nvs_init();
     wifi_init();
     app_init();
 
